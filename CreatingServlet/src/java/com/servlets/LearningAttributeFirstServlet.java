a765d91cbd14951b6d0d2a8229c8c7393342746d
ViurAgarwal25 <geurecruitviduragarwal@gmail.com>
2022-11-27 22:26:03 +0530

    Learning Attributes

---
+++ CreatingServlet/src/java/com/servlets/LearningAttributeFirstServlet.java
@@ -0,0 +1,69 @@
+
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Asus
+ */
+public class LearningAttributeFirstServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LearningAttributeFirstServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            String a=request.getParameter("a");
+            String b=request.getParameter("b");
+            int a_int=Integer.parseInt(a);
+            int b_int=Integer.parseInt(b);
+            int s=a_int+b_int;
+            System.out.println(s);
+            request.setAttribute("sum", s);
+            out.println("</body>");
+            out.println("</html>");
+            //forwarding request with help of request dispatcher
+            RequestDispatcher reqdis=request.getRequestDispatcher("LearningAttributeSecondServlet");
+            reqdis.forward(request,response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CreatingServlet/src/java/com/servlets/LearningAttributeSecondServlet.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Asus
+ */
+public class LearningAttributeSecondServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LearningAttributeSecondServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello To Second Servlet in LearningAttributes</h1>");
+             int a=Integer.parseInt(request.getParameter("a"));
+            int b=Integer.parseInt(request.getParameter("b"));
+            int p=a*b;
+            System.out.println("Product is "+p);
+            
+          int s=(int) request.getAttribute("sum");
+            
+            out.println("<h1>");
+            
+            out.println("Product is "+p);
+            out.println("<br>");
+            out.println("Sum is "+s);
+            out.println("</h1>");
+          
+           
+            out.println("</body>");
+            out.println("</html>");
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
